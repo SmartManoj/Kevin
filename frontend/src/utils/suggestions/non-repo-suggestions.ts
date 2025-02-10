be960ccc5021f9f1af6045465ef7997b926c83d9
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-02-10 14:49:49 +0530

    use simple examples

--- frontend/src/utils/suggestions/non-repo-suggestions.ts
+++ frontend/src/utils/suggestions/non-repo-suggestions.ts
@@ -1,26 +1,13 @@
 import { I18nKey } from "#/i18n/declaration";
 
 const KEY_1 = I18nKey.SUGGESTIONS$HACKER_NEWS;
-const VALUE_1 = `Please write a bash script which displays the top story on Hacker News. It should show the title, the link, and the number of points.
-The script should only use tools that are widely available on unix systems, like curl and grep.`;
+const VALUE_1 = `Please write a python script which displays the top story on Hacker News using v0 api. It should show the title, the link, and the number of points.`;
 
-const KEY_2 = I18nKey.SUGGESTIONS$HELLO_WORLD;
-const VALUE_2 = `I want to create a Hello World app in Javascript that:
-* Displays Hello World in the middle.
-* Has a button that when clicked, changes the greeting with a bouncing animation to fun versions of Hello.
-* Has a counter for how many times the button has been clicked.
-* Has another button that changes the app's background color.`;
+const KEY_2 = `Fetch Trending Github Repositories`;
+const VALUE_2 = `Fetch and Display the Latest Top 5 Trending Repositories on GitHub.`;
 
-const KEY_3 = I18nKey.SUGGESTIONS$TODO_APP;
-const VALUE_3 = `I want to create a VueJS app that allows me to:
-* See all the items on my todo list
-* add a new item to the list
-* mark an item as done
-* totally remove an item from the list
-* change the text of an item
-* set a due date on the item
-
-This should be a client-only app with no backend. The list should persist in localStorage.`;
+const KEY_3 = `Print Bitcoin Price in INR`;
+const VALUE_3 = `Print Bitcoin Price in INR`;
 
 export const NON_REPO_SUGGESTIONS: Record<string, string> = {
   [KEY_1]: VALUE_1,
