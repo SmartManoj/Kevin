5ba84ad2dc6c7cae31c013058428a4f01eaa350f
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-02-11 12:16:42 +0530

    Update llm_checker.py

--- scripts/llm_checker.py
+++ scripts/llm_checker.py
@@ -19,7 +19,11 @@ model = config['model']
 print('Using model:', model)
 api_key = config.get('api_key')
 base_url = config.get('base_url')
-seed = config.get('seed')
+print('Using base_url:', base_url)
+seed = config.get('seed', 42)
+temperature = config.get('temperature', 0)
+print('Using temperature:', temperature, 'and seed:', seed)
+print('-' * 100)
 args = (
     {
         'base_url': base_url,
@@ -29,17 +33,20 @@ args = (
 )
 if base_url and model.startswith('ollama'):
     os.environ['OLLAMA_API_BASE'] = base_url
-has_vision = litellm.supports_vision(model)
-if has_vision:
-    print('Model supports vision')
-else:
-    print('Model does not support vision')
+if 0:
+    has_vision = litellm.supports_vision(model)
+    if has_vision:
+        print('Model supports vision')
+    else:
+        print('Model does not support vision')
 response = litellm.completion(
     model=model,
     messages=[{'role': 'user', 'content': 'Tell a random number with 4 decimal places between 1 to 10.'}],
     api_key=api_key,
     **args,
     seed=seed,
+    temperature=temperature,
 )
 
 print(response.choices[0].message.content)
+# print(response)
