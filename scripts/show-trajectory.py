abc930697f9d23afa6648e0c84dcf0db5d006851
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-02-22 05:52:44 +0530

    Update show-trajectory.py

--- scripts/show-trajectory.py
+++ scripts/show-trajectory.py
@@ -1,15 +1,26 @@
 import requests
 import json
+if 1:
+    json_data = {
+        'feedback_id': '4dbb93310608f43026c9843cf184ce93240b31565f6eb913fbcda369d43ec639',
+    }
 
-json_data = {
-    'feedback_id': '4dbb93310608f43026c9843cf184ce93240b31565f6eb913fbcda369d43ec639',
-}
+    response = requests.post(
+        'https://show-od-trajectory-3u9bw9tx.uc.gateway.dev/show-od-trajectory',
+        json=json_data,
+    )
 
-response = requests.post(
-    'https://show-od-trajectory-3u9bw9tx.uc.gateway.dev/show-od-trajectory',
-    json=json_data,
-)
+    with open('response.json', 'w') as f:
+        data = response.json()
+        json.dump(data, f)
+else:
+    with open('response.json', 'r') as f:
+        data = json.load(f)
+for i in data['trajectory']:
+    if i.get('action') == 'message':
+        print(i['args']['content'])
+    else:
+        print(i)
+    print('-'*100)
 
-with open('response.json', 'w') as f:
-    json.dump(response.json(), f)
 
