0ba275f34e8f4c3c19812d151673b5e5d33efd4e
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-02-28 16:09:51 +0530

    set max_completion_tokens only for o3 models

--- openhands/llm/llm.py
+++ openhands/llm/llm.py
@@ -214,7 +214,6 @@ class LLM(RetryMixin, DebugMixin, CondenserMixin):
         # set up the completion function
         kwargs: dict[str, Any] = {
             'temperature': self.config.temperature,
-            'max_completion_tokens': self.config.max_output_tokens,
         }
         if (
             self.config.model.lower() in REASONING_EFFORT_SUPPORTED_MODELS
@@ -227,7 +226,6 @@ class LLM(RetryMixin, DebugMixin, CondenserMixin):
         # Azure issue: https://github.com/All-Hands-AI/OpenHands/issues/6777
         if self.config.model.startswith('azure'):
             kwargs['max_tokens'] = self.config.max_output_tokens
-            kwargs.pop('max_completion_tokens')
 
         # if o3-mini or o3-mini-2025-01-31, add max_completion_tokens
         if self.config.model.split('/')[-1] in ['o3-mini', 'o3-mini-2025-01-31']:
