e5361f3b7a6e0ca87abe0fa747168f0d23292dd8
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-03-16 14:22:17 +0530

    Fix max_completion_tokens
    model = "groq/qwen-2.5-coder-32b"
    
    litellm.exceptions.BadRequestError: litellm.BadRequestError: OpenAIException - Error code: 400 - {'type': 'urn:inference-service:problem-details:bad-request', 'title': 'Bad Request', 'status': 400, 'detail': "[{'type': 'extra_forbidden', 'loc': ('body', 'max_completion_tokens'), 'msg': 'Extra inputs are not permitted', 'input': 4096}]"}

--- openhands/llm/llm.py
+++ openhands/llm/llm.py
@@ -149,7 +149,7 @@ class LLM(RetryMixin, DebugMixin):
                 'temperature'
             )  # temperature is not supported for reasoning models
         # Azure issue: https://github.com/All-Hands-AI/OpenHands/issues/6777
-        if self.config.model.startswith('azure'):
+        if self.config.model.startswith('azure') or 1:
             kwargs['max_tokens'] = self.config.max_output_tokens
             kwargs.pop('max_completion_tokens')
 
