2f1cf4e58351d9d8fe278f00b0080c6221a9983d
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-04-02 12:00:07 +0530

    reorder

--- containers/runtime/config.sh
+++ containers/runtime/config.sh
@@ -1,6 +1,6 @@
 DOCKER_REGISTRY=ghcr.io
-DOCKER_BASE_DIR="./containers/runtime"
 DOCKER_IMAGE=kevin-sandbox
+DOCKER_BASE_DIR="./containers/runtime"
 # These variables will be appended by the runtime_build.py script
 # DOCKER_IMAGE_TAG=
 # DOCKER_IMAGE_SOURCE_TAG=
