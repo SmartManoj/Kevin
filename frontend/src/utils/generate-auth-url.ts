a2b069182df2b8633013861e3b2e70f48ae298db
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-04-18 11:59:36 +0530

    mc

--- frontend/src/utils/generate-auth-url.ts
+++ frontend/src/utils/generate-auth-url.ts
@@ -4,8 +4,8 @@
  * @param requestUrl The URL of the request
  * @returns The URL to redirect to for OAuth
  */
-export const generateGitHubAuthUrl = (clientId: string, requestUrl: URL) => {
+export const generateAuthUrl = (identityProvider: string, requestUrl: URL) => {
   const redirectUri = `${requestUrl.origin}/api/user/callback`;
   const scope = "repo,user,workflow,offline_access";
-  return `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
+  return `https://github.com/login/oauth/authorize?client_id=${identityProvider}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
 };
