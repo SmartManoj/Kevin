48dd8bc90c2a8e5041c15c93099b175b9dabbf90
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-04-21 19:26:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-DEBUG=1 poetry run uvicorn openhands.server.listen:app --host 0.0.0.0 --port 3000 --ssl-keyfile "./localhost+2-key.pem" --ssl-certfile "./localhost+2.pem"  --reload
+if [ -z "$HTTPS" ]; then
+poetry run uvicorn openhands.server.listen:app --host 0.0.0.0 --port 8000 
+else
+poetry run uvicorn openhands.server.listen:app --host 0.0.0.0 --port 8000 --ssl-keyfile "./localhost+2-key.pem" --ssl-certfile "./localhost+2.pem"  --reload
+fi
