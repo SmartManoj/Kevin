d87e8b611135b7927e1815e478996bcb5b303f1c
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-04-22 05:48:02 +0530

    whitespace changes from rename script

---
+++ frontend/scripts/rename.py
@@ -0,0 +1,14 @@
+f=r'frontend\src\i18n\translation.json'
+
+import json
+
+with open(f, 'r', encoding='utf-8') as file:
+    data = json.load(file, strict=False)
+
+if 0:
+    for key, value in data.items():
+        if 'OpenHands' in value['en']:
+            data[key]['en'] = value['en'].replace('OpenHands', 'Kevin')
+
+with open(f, 'w', encoding='utf-8') as file:
+    json.dump(data, file, indent=4, ensure_ascii=False)
