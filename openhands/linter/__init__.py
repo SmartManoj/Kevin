3903975f7653ddb8fb10c092663048fe78d9f7a9
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-04-30 20:51:37 +0530

    revert

--- openhands/linter/__init__.py
+++ openhands/linter/__init__.py
@@ -6,8 +6,6 @@ code](https://github.com/paul-gauthier/aider/blob/main/aider/linter.py)).
 - The detailed implementation of the linter can be found at: https://github.com/All-Hands-AI/openhands-aci.
 """
 
-# from openhands_aci.linter import DefaultLinter, LintResult
-from openhands.linter.base import LintResult
-from openhands.linter.linter import DefaultLinter
+from openhands_aci.linter import DefaultLinter, LintResult
 
 __all__ = ['DefaultLinter', 'LintResult']
