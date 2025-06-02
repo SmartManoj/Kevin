c253a29a3b6d24c18b5a4a0b6e71657284014b27
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-06-02 14:37:09 +0530

    Update exec.py

--- openhands/runtime/impl/daytona/exec.py
+++ openhands/runtime/impl/daytona/exec.py
@@ -1,20 +1,31 @@
 import os
-from daytona_sdk import Daytona, DaytonaConfig
+from daytona_sdk import CreateSandboxParams, Daytona, DaytonaConfig
 from dotenv import load_dotenv
 
 load_dotenv()
 
-daytona = Daytona(DaytonaConfig(api_key=os.getenv('DAYTONA_ API_KEY')))
-sandbox_id = 'd58beaad-49d3-4bf0-ad0b-3d0cd40dcf3e'
-sandbox = daytona.get_current_sandbox(sandbox_id)
+daytona = Daytona(DaytonaConfig(api_key=os.getenv('DAYTONA_API_KEY')))
+image = 'docker.all-hands.dev/all-hands-ai/runtime:0.40-nikolaik'
+image = 'ghcr.io/smartmanoj/kevin-sandbox:oh_v0.40.0_p3hl9v56jrfymbsz_y49eklt66h256yhr'
+if 1:
+    sandbox = daytona.create(
+        CreateSandboxParams(
+            image=image,
+            public=True,
+            env_vars={
+                'DEBUG': 'true',
+            },
+        )
+    )
+else:
+    sandbox_id = 'd58beaad-49d3-4bf0-ad0b-3d0cd40dcf3e'
+    sandbox = daytona.get_current_sandbox(sandbox_id)
 
 def exec_command(command):
-    while 1:
-        command = input('Enter command: ')
-        response = sandbox.process.exec(
-            command,  timeout=10
-        )
-        print(response.result)
+    response = sandbox.process.exec(
+        command,  timeout=10
+    )
+    print(response.result)
 
 if __name__ == "__main__":
     exec_command('pwd')
