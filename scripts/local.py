4727bd03d8e65bede941d00cedbcecd9875e6fb8
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-06-25 09:01:20 +0530

    Create local.py

---
+++ scripts/local.py
@@ -0,0 +1,2 @@
+a= ['poetry', 'run', 'python', '-u', '-m', 'openhands.runtime.action_execution_server', '31234', '--working-dir', '/mnt/c/Users/smart/Desktop/GD/Kevin/workspace', '--plugins', 'agent_skills', 'jupyter', 'vscode', '--username', 'smart', '--user-id', '1000']
+print(' '.join(a))
